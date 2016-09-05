ff2a79da0bcdf442c70ee4c1f8298570a92cee8e
anil1210 <anil@BEANILKU.in.ibm.com>
2016-09-06 00:09:49 +0530

    modals are added

--- src/main/resources/static/js/appjs/settings.js
+++ src/main/resources/static/js/appjs/settings.js
@@ -4,23 +4,34 @@ roomExpApp.controller('settingCtrl', function($scope, $http) {
 			"July", "August", "September", "October", "November", "December" ];
 
 	var d = new Date();
-	var month = monthNames[d.getMonth()];
+	var day = d.getUTCDate();
+	var firstDay = d.getDay();
+	var month = monthNames[d.getMonth() - 1];
 	$scope.monthName = function() {
 		return month;
 	}
 
-	
-	
+	$scope.from = function() {
+		return firstDay;
+	}
+
+	$scope.enable = function() {
+		console.log(day);
+		if (day <= 10) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	$scope.showAlert = function() {
 		$http({
 			method : 'GET',
 			url : '/resetDB'
 		}).then(function successCallback(response) {
 			$scope.msg = response.data;
-			console.log($scope.msg);
 		}, function errorCallback(response) {
 			$scope.msg = response.data;
-			console.log($scope.msg);
 		});
 	}
 
